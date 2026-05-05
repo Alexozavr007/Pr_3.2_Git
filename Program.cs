32e177497a4d8566f4b8cc593d7e86f2c28d23b3
Кондратюк Олександр <alexozavr007@gmail.com>
2026-05-05 17:32:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.InputEncoding = System.Text.Encoding.UTF8;
+bool e = true;
+Console.WriteLine("Введіть масив для обробки");
+//var arr = ReadJaggedRandom();
+while (e) {
+    Console.WriteLine("Введіть блок для виконання завдання ( 1, 2, 3 або 4 )");
+    var num = Console.ReadLine();
+    switch (num) {
+        case "1":
+            //Block_2_Max(ref arr);
+            break;
+        case "2":
+            //Block_2_Alex(ref arr);
+            break;
+        case "3":
+            //Block_2_Illia(ref arr);
+            break;
+        case "4":
+            //Block_2_Sania(ref arr);
+            break;
+        default:
+            Console.WriteLine("Ви ввели щось не зрозуміле (((");
+            break;
+    }
+    Console.WriteLine("Чи продовжувати далі змінювати масив? (true or false)");
+    e = bool.Parse(Console.ReadLine());
+}
